296a600e73605cfa2acc450bacb5f630427227aa
Brandon Zhu <brandonwz815@gmail.com>
2024-01-09 21:57:11 -0500

    initial commit

---
+++ src/main/java/com/deweylife/deweylifeapi/DeweylifeApiApplication.java
@@ -0,0 +1,13 @@
+package com.deweylife.deweylifeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeweylifeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeweylifeApiApplication.class, args);
+	}
+
+}
